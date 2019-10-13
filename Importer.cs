c76d756bf2f77651187f6b1682ddb7e308cb267b
Barış Kurtlutepe <barisk@gmail.com>
2019-10-14 00:28:02 +0200

    Add project files.

---
+++ Importer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NightFlux
+{
+    class Importer
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NightFlux
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
